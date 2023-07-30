1c4afa0460e7099e96d0080754e3b70cee0647c0
MSiesto <miguelsiesto@gmail.com>
2023-07-30 14:04:56 +0200

    typed state in App.tsx & pass it to InputField.tsx

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { SetStateAction } from 'react'
 import './componentStyles.css'
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({todo, setTodo}: Props) => {
   return (
    <form className='input'>
-    <input type='input' placeholder='Enter a task' className='input__box'></input>
+    <input type='input'
+    value={todo}
+    onChange={
+        (e)=>setTodo(e.target.value)
+    }
+     placeholder='Enter a task' 
+     className='input__box'></input>
    <button className='input__submit' type='submit'>
     Go
    </button>
